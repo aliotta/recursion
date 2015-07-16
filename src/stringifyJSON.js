05b0da72b6f5e18b87100f5f0a22e96b570a3ea8
Alexander Liotta <aliotta343@gmail.com>
2015-07-15 17:50:10 -0700

    "Fleshed out the begging of stringyify jason made it work for simple cases number and string inputs"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,20 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var output = "";
+  while (true){
+	  if(typeof obj === "undefined" || typeof obj==="symbol"){
+		  
+	  } else if (typeof obj === "object"){
+		  
+	  } else if (typeof obj === "array"){
+		  
+	  }
+	  else if (typeof obj === "string"){
+		  return "".concat("'" + obj + "'");
+	  } else {
+		  return "".concat(obj)
+	  }
+  }
+  return output;
 };
