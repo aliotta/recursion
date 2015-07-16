13d1943e2c2fc67ba2506232a19d6b9faf0e6573
Alexander Liotta <aliotta343@gmail.com>
2015-07-15 19:24:55 -0700

    "added a function isFunction that checks whether an object is a function"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,36 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-	var output = null;
-  while (true){
-	  if(typeof obj === "undefined" || typeof obj==="symbol"){ 
-	  } else if (typeof obj === "object" && Array.isArray(obj) === false){
-		  var numKeys = Object.keys(obj).length;
-		  var count = 1;
-		  var objectString = "\{";
-		  for (key in obj){
-			  objectString = objectString.concat(stringifyJSON(key)).concat("\:").concat(stringifyJSON(obj[key]));
-			  if(count != numKeys){
-				  objectString = objectString.concat("\,");
-			  }
-			  count += 1;
-		  }
-		  return objectString.concat("\}")
-	  } else if (typeof obj === "object" && Array.isArray(obj) === true){
-		  var arrayString = "\[";
-		  for (var i = 0; i < obj.length; i++){
-			  arrayString = arrayString.concat(stringifyJSON(obj[i]));
-			  if (i != obj.length -1){
-				  arrayString = arrayString.concat("\,");
-			  }
-		  }
-		  return arrayString.concat("\]")
-	  }
-	  else if (typeof obj === "string"){
-		  return "".concat("\"" + obj + "\"");
-	  } else {
-		  return "".concat(obj)
-	  }
-  }
-  return output;
+    var output = null;
+	function isFunction(functionToCheck) {
+		var getType = {};
+		return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+	}
+    while (true){
+	    if(typeof obj === "undefined" || typeof obj==="symbol"){ 
+	    } else if (typeof obj === "object" && Array.isArray(obj) === false){
+		    var numKeys = Object.keys(obj).length;
+		    var count = 1;
+		    var objectString = "\{";
+		    for (key in obj){
+			    if(obj[key] !== undefined && isFunction(obj[key]) !== true){
+				    objectString = objectString.concat(stringifyJSON(key)).concat("\:").concat(stringifyJSON(obj[key]));
+				    if(count != numKeys){
+					    objectString = objectString.concat("\,");
+			   	    }
+				    count += 1;
+			    }
+		    }
+		    return objectString.concat("\}")
+	    } else if (typeof obj === "object" && Array.isArray(obj) === true){
+		    var arrayString = "\[";
+		    for (var i = 0; i < obj.length; i++){
+			    arrayString = arrayString.concat(stringifyJSON(obj[i]));
+			    if (i != obj.length -1){
+				    arrayString = arrayString.concat("\,");
+			    }
+		    }
+		    return arrayString.concat("\]")
+	    }
+	    else if (typeof obj === "string"){
+		    return "".concat("\"" + obj + "\"");
+	    } else {
+		    return "".concat(obj)
+	    }
+    }
+    return output;
 };
