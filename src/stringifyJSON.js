bf7b4bef646db9cdd7e88b6f57938b1da81cd8c3
Alexander Liotta <aliotta343@gmail.com>
2015-07-15 18:51:11 -0700

    "made stringify work for arrays and fixed some bugs from previous commit"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,17 +7,30 @@ var stringifyJSON = function(obj) {
 	var output = null;
   while (true){
 	  if(typeof obj === "undefined" || typeof obj==="symbol"){ 
-	  } else if (typeof obj === "object"){
+	  } else if (typeof obj === "object" && Array.isArray(obj) === false){
+		  var numKeys = Object.keys(obj).length;
+		  var count = 1;
 		  var objectString = "\{";
 		  for (key in obj){
 			  objectString = objectString.concat(stringifyJSON(key)).concat("\:").concat(stringifyJSON(obj[key]));
+			  if(count != numKeys){
+				  objectString = objectString.concat("\,");
+			  }
+			  count += 1;
 		  }
 		  return objectString.concat("\}")
-	  } else if (typeof obj === "array"){
-		  
+	  } else if (typeof obj === "object" && Array.isArray(obj) === true){
+		  var arrayString = "\[";
+		  for (var i = 0; i < obj.length; i++){
+			  arrayString = arrayString.concat(stringifyJSON(obj[i]));
+			  if (i != obj.length -1){
+				  arrayString = arrayString.concat("\,");
+			  }
+		  }
+		  return arrayString.concat("\]")
 	  }
 	  else if (typeof obj === "string"){
-		  return "".concat("'" + obj + "'");
+		  return "".concat("\"" + obj + "\"");
 	  } else {
 		  return "".concat(obj)
 	  }
